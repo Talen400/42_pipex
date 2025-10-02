629b59396c811a333c16df42361ebcc04ad71fb6
Talen Lavareda Pinto <TalenGitHub0@gmail.com>
2025-10-02 18:57:12 -0300

    testing dup and dup2

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tlavared <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 23:09:31 by tlavared          #+#    #+#             */
-/*   Updated: 2025/10/02 02:13:07 by tlavared         ###   ########.fr       */
+/*   Updated: 2025/10/02 18:14:36 by tlavared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	main(void)
 {
 	int	fd;
+	int	fd_cp;
 
 	fd = open("test.txt" ,O_RDWR);
 	if (fd < 0)
@@ -22,16 +23,18 @@ int	main(void)
 		perror("Error fd < 0");
 		return (1);
 	}
+	fd_cp = dup(fd);
+	printf("fd value: %d\n", fd);
 	if (close(fd))
 	{
 		printf("%s\n", strerror(errno));
 		printf("errno: %d\n", errno);
 		return (1);
 	}
-	if (access("test.txt", F_OK | R_OK) == 0)
-		printf("Existe e lido!\n");
-	else
-		printf("Não existe e não lido : <\n");
-	printf("Passou :>");
+	printf("Lida do fd_cp: %s\n", get_next_line(fd_cp));
+	printf("fd_cp value: %d\n", fd_cp);
+	if (close(fd_cp))
+		perror("Error fd_cp");
+	printf("Passou :>\n");
 	return (0);
 }
